5a2a9c3b7333e119409aa3c7267fe598e87c1ac5
frankjony17 <frankjony17@gmail.com>
2019-06-12 22:51:08 -0300

    Fk001 Python Prometheus e Grafana

---
+++ test_app.py
@@ -0,0 +1,14 @@
+import requests
+import time
+import random
+
+if __name__ == '__main__':
+
+    while True:
+        r_per_batch = random.randint(250, 400)
+        sleep_time = random.uniform(0.1, 1.9)
+        print("Making {} requests after {} seconds".format(r_per_batch, sleep_time))
+        for i in range(r_per_batch):
+           requests.get('http://localhost:5000/labels')
+        time.sleep(sleep_time)
+
